3e9799d86987d2e0953419bf739033c483220809
Adam Letch <izzypod5@googlemail.com>
2020-12-12 16:15:31 +0000

    styling components

--- src/App.js
+++ src/App.js
@@ -45,14 +45,6 @@ class App extends Component {
   };
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-    };
-
     let persons = null;
 
     if (this.state.showPersons) {
@@ -71,13 +63,26 @@ class App extends Component {
           })}
         </div>
       );
+      /*       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black',
+      }; */
+    }
+
+    let classes = [];
+    if (this.state.persons.length < 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button className="button" onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
         {persons}
