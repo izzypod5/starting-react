38e2f82345730ac9adf7c6658d2caa6f2092aba8
Adam Letch <izzypod5@googlemail.com>
2020-12-15 14:23:34 +0000

    Using fragment/context with react internals

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,35 +1,73 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import classes from './App.css';
+import withClass from '../hoc/WithClass';
+import AuthContext from '../context/auth-context';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
-      { id: 'asdf1', name: 'Adam', age: 26 },
-      { id: 'asddf2', name: 'Mona', age: 26 },
-      { id: 'asfgf3', name: 'Max', age: 28 },
+      { id: 'asfa1', name: 'Adam', age: 26 },
+      { id: 'vasdf1', name: 'Mona', age: 26 },
+      { id: 'asdf11', name: 'Max', age: 28 },
     ],
     otherState: 'some other value',
     showPersons: false,
+    showCockpit: true,
+    changeCounter: 0,
+    authenticated: false,
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  // componentWillMount() {
+  //   console.log('[App.js] componentWillMount');
+  // }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex((p) => {
-      return p.userId === id;
+      return p.id === id;
     });
 
     const person = {
       ...this.state.persons[personIndex],
     };
+
     // const person = Object.assign({}, this.state.persons[personIndex]);
+
     person.name = event.target.value;
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({
-      persons: persons,
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1,
+      };
     });
   };
 
@@ -45,7 +83,12 @@ class App extends Component {
     this.setState({ showPersons: !doesShow });
   };
 
+  loginHandler = () => {
+    this.setState({ authenticated: true });
+  };
+
   render() {
+    console.log('[App.js] render');
     let persons = null;
 
     if (this.state.showPersons) {
@@ -54,26 +97,40 @@ class App extends Component {
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
           changed={this.nameChangedHandler}
+          isAuthenticated={this.state.authenticated}
         />
       );
     }
 
     return (
-      <div className={classes.App}>
-        <Cockpit
-          showPersons={this.state.showPersons}
-          persons={this.state.persons}
-          clicked={this.togglePersonsHandler}
-        />
-        {persons}
-      </div>
+      <Fragment>
+        <button
+          onClick={() => {
+            this.setState({ showCockpit: false });
+          }}
+        >
+          Remove Cockpit
+        </button>
+        <AuthContext.Provider
+          value={{
+            authenticated: this.state.authenticated,
+            login: this.loginHandler,
+          }}
+        >
+          {this.state.showCockpit ? (
+            <Cockpit
+              title={this.props.appTitle}
+              showPersons={this.state.showPersons}
+              personsLength={this.state.persons.length}
+              clicked={this.togglePersonsHandler}
+            />
+          ) : null}
+          {persons}
+        </AuthContext.Provider>
+      </Fragment>
     );
-    /*     return React.createElement(
-      'div',
-      null,
-      React.createElement('h1', { className: 'App' }, 'Does this work now?')
-    ); */
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
